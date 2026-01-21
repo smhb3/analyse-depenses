fb2e8d98b378497e4ee1752f2af3d16df222e160
Sofiene MEHABLIA <sofienesup@gmail.com>
2026-01-21 18:57:28 +0100

    v1.0 - Analyse CSV et regroupements par categories

---
+++ main.py
@@ -0,0 +1,27 @@
+categories = {
+    "Nourriture"  : 0,
+    "Transport"   : 0,
+    "Abonnements" : 0,
+    "Revenus"     : 0,
+    "Autre"       : 0
+}
+
+with open("transactions.csv", "r", encoding="utf-8") as f:
+    f.readline()        
+    for ligne in f :
+        l = ligne.strip()   
+        l = l.split(",") 
+        montant = float(l[2])
+        if montant > 0 :
+            categories["Revenus"] += montant
+        elif ("UBER" in l[1]) or ("CARREFOUR" in l[1]):
+            categories["Nourriture"] += montant
+        elif ("RATP" in l[1]):
+            categories["Transport"] += montant
+        elif ("NETFLIX" in l[1]):
+            categories["Abonnements"] += montant
+        else :
+            categories["Autre"] += montant
+
+for cle in categories :
+    print(cle, " : ", f"{categories[cle]:.2f}")
